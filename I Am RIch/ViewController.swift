f9cbdaeebe48316ad015f4b26666fa45504f3835
contrbas@gmail.com <K4rmU78B>
2020-02-08 11:59:04 +0200

    hello world app

---
+++ I Am RIch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am RIch
+//
+//  Created by Oleksii TIMOFIEIEV on 2/8/20.
+//  Copyright © 2020 Oleksii TIMOFIEIEV. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
